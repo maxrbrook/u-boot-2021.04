108615f84dba390939729f8a5536f8ff1c4882b3
maxrbrook <135770252+maxrbrook@users.noreply.github.com>
2023-06-16 11:25:46 +1000

    Update axp305.c
    Added in the DCDC3_1200mv changes for testing the MangoPi

--- drivers/power/axp305.c
+++ drivers/power/axp305.c
@@ -16,7 +16,7 @@
 #include <axp_pmic.h>
 
 #define AXP305_DCDC4_1600MV_OFFSET 46
-
+#define AXP305_DCDC3_1200MV_OFFSET 71
 static u8 axp305_mvolt_to_cfg(int mvolt, int min, int max, int div)
 {
 	if (mvolt < min)
@@ -31,12 +31,38 @@ int axp_set_dcdc4(unsigned int mvolt)
 {
 	int ret;
 	u8 cfg;
+	/*
+	if 0
+		if (mvolt >= 1600)
+			cfg = AXP305_DCDC4_1600MV_OFFSET +
+				axp305_mvolt_to_cfg(mvolt, 1600, 3300, 100);
+		else
+			cfg = axp305_mvolt_to_cfg(mvolt, 600, 1500, 20);
 
-	if (mvolt >= 1600)
-		cfg = AXP305_DCDC4_1600MV_OFFSET +
-			axp305_mvolt_to_cfg(mvolt, 1600, 3300, 100);
+		if (mvolt == 0)	
+			return pmic_bus_clrbits(AXP305_OUTPUT_CTRL1,
+					AXP305_OUTPUT_CTRL1_DCDCD_EN);
+
+		ret = pmic_bus_write(AXP305_DCDCD_VOLTAGE, cfg);
+		if (ret)
+			return ret;
+	
+		return pmic_bus_setbits(AXP305_OUTPUT_CTRL1,
+					AXP305_OUTPUT_CTRL1_DCDCD_EN);
+	*/
+	return 0;
+}
+
+int axp_set_dcdc3(unsigned int mvolt)
+{
+	int ret;
+	u8 cfg;
+	
+	if (mvolt >= 1220)
+		cfg = AXP305_DCDC3_1200MV_OFFSET +
+			axp305_mvolt_to_cfg(mvolt, 1220, 1840, 20);
 	else
-		cfg = axp305_mvolt_to_cfg(mvolt, 600, 1500, 20);
+		cfg = axp305_mvolt_to_cfg(mvolt, 500, 1200, 10);
 
 	if (mvolt == 0)
 		return pmic_bus_clrbits(AXP305_OUTPUT_CTRL1,
@@ -45,11 +71,10 @@ int axp_set_dcdc4(unsigned int mvolt)
 	ret = pmic_bus_write(AXP305_DCDCD_VOLTAGE, cfg);
 	if (ret)
 		return ret;
-
+	
 	return pmic_bus_setbits(AXP305_OUTPUT_CTRL1,
-				AXP305_OUTPUT_CTRL1_DCDCD_EN);
+				0x1f);
 }
-
 int axp_init(void)
 {
 	u8 axp_chip_id;
@@ -63,13 +88,20 @@ int axp_init(void)
 	if (ret)
 		return ret;
 
-	if ((axp_chip_id & AXP305_CHIP_VERSION_MASK) != 0x40)
+	//if ((axp_chip_id & AXP305_CHIP_VERSION_MASK) != 0x40)
+	//	return -ENODEV;
+	
+	if ((axp_chip_id & AXP305_CHIP_VERSION_MASK) != 0x4b)
 		return -ENODEV;
-
+		
+	printf("pmic id is 0x%x\n",axp_chip_id);
+	
+	axp_set_dcdc3(1500);
+	
 	return ret;
 }
 
-#ifndef CONFIG_PSCI_RESET
+#if !CONFIG_IS_ENABLED(ARM_PSCI_FW) && !IS_ENABLED(CONFIG_SYSRESET_CMD_POWEROFF)
 int do_poweroff(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	pmic_bus_write(AXP305_SHUTDOWN, AXP305_POWEROFF);
